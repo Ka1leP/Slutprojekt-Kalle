183191e204d12ef53fd67b948578ba736887c94d
Ka1leP <116098809+Ka1leP@users.noreply.github.com>
2023-05-15 20:58:02 +0200

    Fixat buggar och snyggat till lite

--- js/apply.js
+++ js/apply.js
@@ -1,15 +1,15 @@
 function set_show_medborgarskap_input(val){
     var element=document.getElementById('medborgarskap_input');
-    if (val=="other"){
+    if (val=="other"){ //när other är valt så kommer rutan upp
         element.style.display='';
         element.value='';
     }
     else{
-        element.style.display='none';
+        element.style.display='none'; //när det inte är "other " så visas inget
         element.value=val;
     }
 }
  
 
  set_show_medborgarskap_input(document.getElementById('medborgarskap').value); 
- //gör så att raden göms även när man startar/laddar om 
+ //gör så att raden göms även när man startar/laddar om, innan var det här en bugg och är nu löst
